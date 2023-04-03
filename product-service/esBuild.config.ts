cec92fe8dda8759c2eb6d5befc5e43876ab30ae0
Anastasiya Antonenka <anastasiya_antonenka@epam.com>
2023-04-03 22:50:53 +0300

    feat: add automatic compilation

--- product-service/esBuild.config.ts
+++ product-service/esBuild.config.ts
@@ -7,4 +7,8 @@ export default {
     define: { 'require.resolve': undefined },
     platform: 'node',
     concurrency: 10,
+    watch: {
+      pattern: ['src/**/*.ts'],
+      ignore: ['temp/**/*']
+    }
 };
